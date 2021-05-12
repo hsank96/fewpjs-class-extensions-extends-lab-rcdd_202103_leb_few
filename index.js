e2f3d7411d4a4735f87cf4cea94f4c53cf77e1d3
Hazem El Sankari <hazemleb96@gmail.com>
2021-05-12 11:44:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+class Polygon{
+  constructor(arr){
+    this.arr = arr;
+  }
+  get countSides(){
+    return this.arr.length;
+  }
+  get perimeter(){
+    let p=0;
+    for (let i=0; i<this.arr.length; i++){
+      p+= this.arr[i]
+    }
+    return p;
+  }
+}
+
+class Triangle extends Polygon{}
