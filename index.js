07070fbfa139e28982d077df91a32b9414a15700
Hazem El Sankari <hazemleb96@gmail.com>
2021-05-12 11:57:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,3 +25,14 @@ class Triangle extends Polygon{
     }
   }
 }
+
+class Square extends Polygon{
+  get isValid(){
+    if(this.arr[0]===this.arr[1] && this.arr[1]===this.arr[2] && this.arr[2] === this.arr[3]){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+}
